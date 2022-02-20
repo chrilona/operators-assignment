e3e68b815f9eda51b6ea4025280e1c83f2128778
chrilona <christabelalona@gmail.com>
2022-02-20 22:23:06 +0300

    second datatypeassignment

---
+++ src/main.kt
@@ -0,0 +1,33 @@
+fun main() {
+    name()
+    sum()
+    modulus()
+    intrestingFact()
+}
+
+fun name(){
+    println("Hello lona")
+
+}
+fun sum(){
+    var no1= 13
+    var no2 = 23
+    var no3 = 33
+    var no4 = 44
+    var sum= (no1+no2+no4+no3)
+    println(sum)
+
+}
+
+fun modulus(){
+
+    var number1= 13
+    var number2 = 6
+    var modulus = (number1%number2)
+    println(modulus)
+}
+
+fun intrestingFact(){
+    var interest = "I over analyze"
+    println(interest)
+}
